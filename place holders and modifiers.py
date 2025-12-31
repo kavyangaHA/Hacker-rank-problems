02b7537a7f533708065f179d41d88e3feb8eb7db
Princess_Cloud <kavyangahathurusinghe1234@gmail.com>
2025-12-31 23:07:31 +0530

    Add files via upload

---
+++ place holders and modifiers.py
@@ -0,0 +1,17 @@
+prize=89
+t=f"the price is {prize} Rs."
+m=f"The prize is {prize:.2f} Rs."
+l=f"The prize is {20*30} Rs."
+print(t)
+print(m)
+print(l)
+j="hjkl"
+t=j.index("h")
+print(t)
+
+
+
+m=f"price{prize: .3f} rs"
+print(m)
+c=f"waw this is awesome{prize:.5f}"
+print(c)
