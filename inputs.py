c3713ce3afdd92e0aa91bac08ffc3e49b3992cd6
Princess_Cloud <kavyangahathurusinghe1234@gmail.com>
2025-12-31 23:06:38 +0530

    Add files via upload

---
+++ inputs.py
@@ -0,0 +1,12 @@
+'''
+print('pls input an integer')
+a=int(input())
+print('pls input a word')
+
+b=input()
+print("pls input something like '12-14-20202'")
+p=input().split("-")
+print("pls input something like '12-14-20202'")
+d=input()
+parts=list(map(int,d.split("-")))'''
+m= list(map(int,input().split("-")))
